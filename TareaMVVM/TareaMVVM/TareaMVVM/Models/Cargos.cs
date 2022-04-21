e84ad4f8d7539e49fb483aff7e1b1718c502d482
tavovilorio <gustavolvilorio04@hotmail.com>
2022-04-20 21:45:43 -0600

    Add files via upload

---
+++ TareaMVVM/TareaMVVM/TareaMVVM/Models/Cargos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaMVVM.Models
+{
+    public class Cargos
+    {
+        public string Cargo { get; set; }
+        public static List<Cargos> GetCargos()
+        {
+            var cargo = new List<Cargos>()
+            {
+                new Cargos(){ Cargo ="Cajero" },
+                new Cargos(){ Cargo ="IT" },
+                new Cargos(){ Cargo ="Auditor" },
+                new Cargos(){ Cargo ="Gerente" },
+                new Cargos(){ Cargo ="Supervisor" }
+            };
+            return cargo;
+        }
+    }
+}
